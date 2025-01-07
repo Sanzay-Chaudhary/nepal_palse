ac38014aeadffc095d297a516d023bf15738a168
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-01-07 13:55:08 +0545

    created home_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nepal_palse/view/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Nepal Palse',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Nepal Palse'),
+      home: const MyHomePage(),
     );
   }
 }
