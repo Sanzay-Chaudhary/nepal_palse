be9e35f1cbf255fe100fbe294a51ad004a935c6d
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-03-27 16:14:40 +0545

    created news_view_model

--- lib/core/view_models/news_viewmodel.dart
+++ lib/core/view_models/news_viewmodel.dart
@@ -1,18 +1,18 @@
-import 'package:flutter/material.dart';
+import '../models/news_model.dart';
 
-class NewsModel extends ChangeNotifier {
-  final List<NewsModel> _newsList = [
-    NewsModel(
-      title: "Breaking News: Something Big Happened!",
-      description: "Here’s a short description of the news article.",
-      imageUrl: "https://source.unsplash.com/random/800x600",
-    ),
-    NewsModel(
-      title: "Tech Update: New Gadgets Released",
-      description: "Latest technology updates from the industry.",
-      imageUrl: "https://source.unsplash.com/random/801x601",
-    ),
-  ];
-
-  List<NewsModel> get newsList => _newsList;
+class NewsViewModel {
+  List<NewsModel> getNews() {
+    return [
+      NewsModel(
+        title: "Breaking News: Something Big Happened!",
+        description: "Here’s a short description of the news article.",
+        imgUrl: "https://source.unsplash.com/random/800x600",
+      ),
+      NewsModel(
+        title: "Tech Update: New Gadgets Released",
+        description: "Latest technology updates from the industry.",
+        imgUrl: "https://source.unsplash.com/random/801x601",
+      ),
+    ];
+  }
 }
