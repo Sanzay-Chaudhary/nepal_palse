12443d78f2ffec49073dd0fa9760c8f06bbc6299
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-04-02 17:12:51 +0545

    created news_view_model

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nepal_palse/core/view_models/news_viewmodel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,11 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Nepal Pulse')));
+    final newsViewModel = NewsViewModel();
+    final newsList = newsViewModel.getNews();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Nepal Pulse')),
+      // body: NewsList(newsList: newsList),
+    );
   }
 }
