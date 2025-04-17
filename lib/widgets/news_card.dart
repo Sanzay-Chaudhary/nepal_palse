7a8a71ef6adf386a251d8660ec2a8d06e45b1955
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-04-17 16:17:33 +0545

    updated news_card

--- lib/widgets/news_card.dart
+++ lib/widgets/news_card.dart
@@ -1,62 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:nepal_palse/core/models/news_model.dart';
-import 'package:nepal_palse/views/news_detail_screen.dart';
 
-class NewsCard extends StatefulWidget {
+class NewsTile extends StatefulWidget {
   final NewsModel news;
-  const NewsCard({super.key, required this.news});
+
+  const NewsTile({super.key, required this.news});
 
   @override
-  State<NewsCard> createState() => _NewsCardState();
+  State<NewsTile> createState() => _NewsTileState();
 }
 
-class _NewsCardState extends State<NewsCard> {
+class _NewsTileState extends State<NewsTile> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => NewsDetailScreen(news: widget.news),
+    return Card(
+      margin: const EdgeInsets.all(12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            widget.news.imgUrl,
+            height: 200,
+            width: double.infinity,
+            fit: BoxFit.cover,
           ),
-        );
-      },
-      child: Card(
-        margin: EdgeInsets.all(8.0),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-              child: Image.network(
-                widget.news.imgUrl,
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: 200,
-              ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.news.title,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                widget.news.title,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Text(
-                widget.news.description,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            const SizedBox(height: 10),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(widget.news.description),
+          ),
+        ],
       ),
     );
   }
