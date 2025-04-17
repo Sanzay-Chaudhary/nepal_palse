181ed5a22e5bd3c8b05c276bb6fd902cab3889f6
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-04-17 16:20:14 +0545

    updated home_screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nepal_palse/core/view_models/news_provider.dart';
-import 'package:nepal_palse/widgets/news_list.dart';
-// import 'package:nepal_palse/core/view_models/news_viewmodel.dart';
+import 'package:nepal_palse/widgets/news_card.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,27 +11,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final NewsViewModel newsViewModel = NewsViewModel();
-
   @override
   void initState() {
     super.initState();
-    fetchNewsData();
-  }
-
-  void fetchNewsData() async {
-    await newsViewModel.getNews();
-    setState(() {}); // Update UI after fetching news
+    Provider.of<NewsProvider>(context, listen: false).fetchNews();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Nepal Pulse")),
-      body:
-          newsViewModel.isLoading
-              ? const Center(child: CircularProgressIndicator())
-              : NewsList(newsList: newsViewModel.newsList),
+      appBar: AppBar(title: const Text('Nepal Pulse')),
+      body: Consumer<NewsProvider>(
+        builder: (context, provider, child) {
+          if (provider.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            itemCount: provider.newsList.length,
+            itemBuilder: (context, index) {
+              return NewsTile(news: provider.newsList[index]);
+            },
+          );
+        },
+      ),
     );
   }
 }
