6fcc500e8e12e9edf05bd61896c87e8f0b06d2e5
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-04-20 17:38:09 +0545

    created news_card

--- lib/widgets/news_card.dart
+++ lib/widgets/news_card.dart
@@ -1,45 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:nepal_palse/core/models/news_model.dart';
-
-// class NewsTile extends StatefulWidget {
-//   final NewsModel news;
-
-//   const NewsTile({super.key, required this.news});
-
-//   @override
-//   State<NewsTile> createState() => _NewsTileState();
-// }
-
-// class _NewsTileState extends State<NewsTile> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//       margin: const EdgeInsets.all(12),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           Image.network(
-//             widget.news.imgUrl,
-//             height: 200,
-//             width: double.infinity,
-//             fit: BoxFit.cover,
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: Text(
-//               widget.news.title,
-//               style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: Text(widget.news.description),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:nepal_palse/core/models/news_model.dart';
 
@@ -55,7 +13,7 @@ class NewsTile extends StatelessWidget {
       child: ListTile(
         contentPadding: const EdgeInsets.all(8.0),
         leading:
-            news.imgUrl != null
+            news.imgUrl.isNotEmpty
                 ? Image.network(
                   news.imgUrl,
                   fit: BoxFit.cover,
@@ -73,7 +31,6 @@ class NewsTile extends StatelessWidget {
           overflow: TextOverflow.ellipsis,
         ),
         onTap: () {
-          // You can handle navigation here if you want to open the full article.
           print('Navigating to: ${news.url}');
         },
       ),
